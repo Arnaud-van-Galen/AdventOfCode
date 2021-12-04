1903225c9cc6495ba70b0f3e9dea501c6e0f1ef7
Arnaud van Galen <arnaud.van.galen@inspark.nl>
2021-12-04 02:17:18 +0100

    Improved 2nd solution by make both findvalues equally easy to understand

--- 2021/03-2.ps1
+++ 2021/03-2.ps1
@@ -1,24 +1,25 @@
-#[string[]]$Report = "00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"
-[string[]]$Report=Get-Content .\03-1-Input.txt
-[int]$DiagLength = ($Report[0]).Length
+# [string[]] $Report = "00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"
+[string[]] $Report = Get-Content .\03-1-Input.txt
 
-#Not happy with all the "if ($findvalue) and "if (!$findvalue)" but it was needed for the -gt vs -ge and to avoid writing seperate Find-MostCommon and Find-LeastCommon functions
-function Find-Common {param([int]$findvalue, $report)
+function Find-MostCommonOrMatching { param( [string] $findvalue, [string[]] $report )
+    [int] $DiagLength = $report[0].Length
     for ($i = 0 ; $i -lt $DiagLength ; $i++) {
-        [string[]]$workingReport = $report | Where-Object {$_.substring($i, 1) -eq $findvalue }
-        if ((!$findvalue -and $workingReport.Length / $report.Length -gt 1/2) -or ($findvalue -and $workingReport.Length / $report.Length -ge 1/2)) {
-            if (!$findvalue) { $workingReport = $report | Where-Object {$_ -notin $workingReport} }
-            #Write-Host "keeping 1"
+        [string[]] $workingReport = $report | Where-Object { $_.substring($i, 1) -eq $findvalue }
+        if ($findvalue -eq "1") {
+            if ($workingReport.Length / $report.Length -lt 1/2) {
+                $workingReport = $report | Where-Object { $_ -notin $workingReport }
+            }
         } else {
-            if ($findvalue) { $workingReport = $report | Where-Object {$_ -notin $workingReport} }
-            #Write-Host "keeping 0"
+            if ($workingReport.Length / $report.Length -gt 1/2) {
+                $workingReport = $report | Where-Object { $_ -notin $workingReport }
+            }
         }
         $report = $workingReport
-        #Write-Host $i, $report
-        if ($report.Length -eq 1) { return [Convert]::ToInt32($report,2)}
+        # Write-Host $i, $report
+        if ($report.Length -eq 1) { return [Convert]::ToInt32($report, 2) }
     }
 }
 
-$OxygenGeneratorRating = Find-Common -findvalue 1 -report $Report
-$CO2ScrubberRating = Find-Common -findvalue 0 -report $Report
+$OxygenGeneratorRating = Find-MostCommonOrMatching -findvalue "1" -report $Report
+$CO2ScrubberRating = Find-MostCommonOrMatching -findvalue "0" -report $Report
 Write-Host ($OxygenGeneratorRating * $CO2ScrubberRating)
