628d8a06ea3b7697d626877da8807f304db88c0f
Arnaud van Galen <arnaud.van.galen@inspark.nl>
2021-12-04 18:10:07 +0100

    Layout improvements and documentation

--- 2021/01-2.ps1
+++ 2021/01-2.ps1
@@ -2,14 +2,14 @@
 [int]$IncreaseCount=0
 [int]$GroupSize=3
 
-#$Report=199,200,208,210,200,207,240,269,260,263
+# $Report=199,200,208,210,200,207,240,269,260,263
 $Report=Get-Content .\01-1-Input.txt
 
 for ($i = $GroupSize ; $i -lt $Report.Count ; $i++) {
     if (!$PrevReportValue) { $PrevReportValue = $Report[($i-$GroupSize)..($i-1)] | Measure-Object -Sum | Select-Object -expand Sum }
     $ReportValue = $Report[($i-$GroupSize+1)..($i)] | Measure-Object -Sum | Select-Object -expand Sum
     if ($PrevReportValue -lt $ReportValue) {
-        #Write-Host "From $PrevReportValue to $ReportValue is an increase"
+        # Write-Host "From $PrevReportValue to $ReportValue is an increase"
         $IncreaseCount++
     }
     $PrevReportValue=$ReportValue
--- 2021/04-2.ps1
+++ 2021/04-2.ps1
@@ -1,4 +1,4 @@
-#[string[]] $BingoInput = Get-Content .\04-1-Demo-Input.txt
+# [string[]] $BingoInput = Get-Content .\04-1-Demo-Input.txt
 [string[]] $BingoInput = Get-Content .\04-1-Input.txt
 [string[]] $BingoOrder = $BingoInput[0].Split(",")
 [System.Collections.ArrayList] $BingoCards = @()
@@ -10,13 +10,16 @@ for ($i = 1 ; $i -lt $BingoInput.Count ; $i++) {
         $BingoCards[-1] += $BingoInput[$i].Split(" ", [System.StringSplitOptions]::RemoveEmptyEntries)
     }
 }
+[int] $BingoSize = [math]::Sqrt($BingoCards[0].Count)
 
 $Bingo = $false
-[int] $BingoSize = [math]::Sqrt($BingoCards[0].Count)
 foreach ($BingoCallNumber in $BingoOrder) {
     foreach ($BingoCard in $BingoCards) {
+        # All fields in the BingoCard should have a value 0 and up.
+        # If the number is Drawn/Hit the value will be set to -1 and Bingo happens when all fields in a row or column become -1
+        # After Bingo on a card all fields are emptied so the card will not have to be checked again 
         if ($BingoCard[0]) {
-        $HitIndex = $BingoCard.IndexOf($BingoCallNumber)
+            $HitIndex = $BingoCard.IndexOf($BingoCallNumber)
             if ($HitIndex -ge 0) {
                 $BingoCard[$HitIndex] = -1
                 $HitRow = [Math]::Floor($HitIndex / $BingoSize)
@@ -27,7 +30,7 @@ foreach ($BingoCallNumber in $BingoOrder) {
                 for ($i = 0 ; $i -lt $BingoCard.Count ; $i++) {
                     if ($i % $BingoSize -eq $HitColumn) { $HitColumnSum += $BingoCard[$i] }
                 }
-                #ToDo: Improve the above loop with Object.Where like $BingoCard.Where({ $_.Rank % $BingoSize -eq $HitColumn })
+                # ToDo: Improve the above loop with Object.Where like $BingoCard.Where({ $_.Rank % $BingoSize -eq $HitColumn })
                 if ($HitColumnSum + $BingoSize -eq 0) { $Bingo = $true }
                 if ($Bingo) {
                     Write-Host "Bingo on Card $BingoCard after $BingoCallNumber"
