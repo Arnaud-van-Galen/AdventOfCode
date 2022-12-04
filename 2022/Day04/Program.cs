2c32670b9b1b96ae04e030b71fdc6f721d509d53
Arnaud van Galen <arnaud.vangalen@ns.nl>
2022-12-04 23:20:26 +0100

    202204AkisPart1Brilliance

--- 2022/Day04/Program.cs
+++ 2022/Day04/Program.cs
@@ -13,7 +13,7 @@ namespace AOC202204
                 // Move the Data-fetching before or after the starting of the stopwatch (Alt + Arrow Up/Down)
                 string[] Data = System.IO.File.ReadAllLines(@"C:\Users\ArnaudvanGalen\Repos\AdventOfCode\AdventOfCode\2022\Day04\Data.txt");
                 System.Diagnostics.Stopwatch stopwatch = System.Diagnostics.Stopwatch.StartNew();
-                int result = Part2(Data); // Change to Part1 or Part2
+                int result = Part1(Data); // Change to Part1 or Part2
                 SpeedTest[i] = stopwatch.Elapsed.TotalMilliseconds;
                 Console.WriteLine(result);
                 // Move the Data-output before or after the reading of the stopwatch (Alt + Arrow Up/Down)
@@ -31,7 +31,8 @@ namespace AOC202204
                 int max1 = Convert.ToInt16(Areas[1]);
                 int min2 = Convert.ToInt16(Areas[2]);
                 int max2 = Convert.ToInt16(Areas[3]);
-                if ((min1 <= min2 && max1 >= max2) || (min2 <= min1 && max2 >= max1))
+                if ((min1 <= min2 && max1 >= max2) || (min2 <= min1 && max2 >= max1)) // Smart code by Arnaud
+                // if ((min1 - min2) * (max1 - max2) <= 0) // Even smarter code by Aki. Multiplications are only > 0 if both factors are positive or negative
                 {
                     OverlappingSum++;
                 }
