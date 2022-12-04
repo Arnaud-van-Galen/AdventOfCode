7f0346fe483ecd318405932958d3c4d68fda4d36
Arnaud van Galen <arnaud.vangalen@ns.nl>
2022-12-04 14:28:00 +0100

    202204

---
+++ 2022/Day04/Part1.ps1
@@ -0,0 +1,17 @@
+Get-MyVariables | Remove-Variable -ErrorAction SilentlyContinue
+[System.Console]::Clear()
+
+[int] $OverlappingSum = 0
+
+# $Data = Get-Content -Path $PSScriptRoot\DataDemo.txt -ErrorAction Stop
+$Data = Get-Content -Path $PSScriptRoot\Data.txt -ErrorAction Stop
+
+foreach ($AssignmentPair in $Data) {
+    [int]$min1, [int]$max1, [int]$min2, [int]$max2 = $AssignmentPair.Split("-,".ToCharArray())
+    if (($min1 -le $min2 -and $max1 -ge $max2) -or ($min2 -le $min1 -and $max2 -ge $max1)) {
+        $OverlappingSum++
+    }
+}
+
+Write-Host "$OverlappingSum"
+# Correct answer = 459 (2 for testdata)
---
+++ 2022/Day04/Part2.ps1
@@ -0,0 +1,17 @@
+Get-MyVariables | Remove-Variable -ErrorAction SilentlyContinue
+[System.Console]::Clear()
+
+[int] $OverlappingSum = 0
+
+# $Data = Get-Content -Path $PSScriptRoot\DataDemo.txt -ErrorAction Stop
+$Data = Get-Content -Path $PSScriptRoot\Data.txt -ErrorAction Stop
+
+foreach ($AssignmentPair in $Data) {
+    [int]$min1, [int]$max1, [int]$min2, [int]$max2 = $AssignmentPair.Split("-,".ToCharArray())
+    if (!($max1 -lt $min2 -or $max2 -lt $min1)) {
+        $OverlappingSum++
+    }
+}
+
+Write-Host "$OverlappingSum"
+# Correct answer = 779 (4 for testdata)
