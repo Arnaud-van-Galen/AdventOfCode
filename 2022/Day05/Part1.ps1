b675d81e47b1cfc0404b7ae2ab0f195821ab45b3
Arnaud van Galen <arnaud.vangalen@ns.nl>
2022-12-06 00:10:20 +0100

    202205 Proper parsing of input and simplifications

--- 2022/Day05/Part1.ps1
+++ 2022/Day05/Part1.ps1
@@ -2,23 +2,22 @@ Get-MyVariables | Remove-Variable -ErrorAction SilentlyContinue
 [System.Console]::Clear()
 
 # $Data = Get-Content -Path $PSScriptRoot\DataDemo.txt -Raw -ErrorAction Stop
-# $Stacks = @("".ToCharArray(), "ZN".ToCharArray(), "MCD".ToCharArray(), "P".ToCharArray()) # ToDo, get this from code
 $Data = Get-Content -Path $PSScriptRoot\Data.txt -Raw -ErrorAction Stop
-$Stacks = @("".ToCharArray(), "SMRNWJVT".ToCharArray(), "BWDJQPCV".ToCharArray(), "BJFHDRP".ToCharArray(), "FRPBMND".ToCharArray(), "HVRPTB".ToCharArray(), "CBPT".ToCharArray(), "BJRPL".ToCharArray(), "NCSLTZBW".ToCharArray(), "LSG".ToCharArray()) # ToDo, get this from code
 
 $DataStacks, $DataInstructions = $Data -split '(?:\r?\n){2,}'
+$StacksUnparsed = $DataStacks.split([System.Environment]::NewLine)
+$Stacks = [ordered] @{}
+# Assumes StackNumbers and Letters begin at position "1", are 4 positions apart and are 1 character long
+for ($StackColumn = 1; $StackColumn -lt $StacksUnparsed[0].Length; $StackColumn += 4) {
+    $Stack = (-join $StacksUnparsed.ForEach{ $_[$StackColumn] }).Trim()
+    $Stacks.Add( $Stack[-1].ToString(), -join $stack[-2..(-1 * $Stack.Length)] )
+}
 $Instructions = $DataInstructions.split([System.Environment]::NewLine)
 foreach ($Instruction in $Instructions) {
     $Amount, $From, $To = $Instruction.split()[1,3,5]
-    $MoveStack = $Stacks[$From][-1..(-1 * $Amount)]
-    $Stacks[$To] += $MoveStack
-    if ($Stacks[$From].Length -gt $Amount) {
-        $Stacks[$From] = $Stacks[$From][0..($Stacks[$From].Length - $Amount -1 )]
-    } else {
-        $Stacks[$From] = @()
-    }
-
+    $Stacks[$To] += -join $Stacks[$From][-1..(-1 * $Amount)]
+    $Stacks[$From] = $Stacks[$From].Substring(0, $Stacks[$From].Length - $Amount)
 }
 
--join $Stacks.ForEach{$_[-1]}
+-join $Stacks.Values.ForEach{$_[-1]}
 # Correct answer = LJSVLTWQM (CMZ for testdata)
