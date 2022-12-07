9b2f8ef1da3a4e577be364f1278b6455b38a3f63
Arnaud van Galen <arnaud.vangalen@ns.nl>
2022-12-07 11:55:54 +0100

    202207 Part2 gaat nog iets fout

---
+++ 2022/Day07/Part1.ps1
@@ -0,0 +1,54 @@
+Get-MyVariables | Remove-Variable -ErrorAction SilentlyContinue
+[System.Console]::Clear()
+
+[string] $Path = "/" # Make sure to always end Path with a /
+[hashtable] $Files = @{}
+[hashtable] $Folders = @{}
+[int] $TotalSizeLimit = 100000
+[int] $TotalSize = 0
+
+# $Data = Get-Content -Path $PSScriptRoot\DataDemo.txt -ErrorAction Stop
+$Data = Get-Content -Path $PSScriptRoot\Data.txt -ErrorAction Stop
+
+foreach ($TerminalLine in $Data) {
+    if ($TerminalLine.StartsWith("$")) {
+        if ($TerminalLine.StartsWith("$ cd")) {
+            $PathChange = $TerminalLine.Split(" ")[2]
+            switch ($PathChange) {
+                "/" {
+                    $Path = "/"
+                    break
+                }
+                ".." {
+                    if ($Path -ne "/") {
+                        $SecondToLastSlash = $Path.Substring(0, $Path.Length - 1).LastIndexOf("/")
+                        $Path = $Path.Substring(0, $SecondToLastSlash) + "/"
+                    }
+                }
+                Default {
+                    $Path += $PathChange + "/"
+                }
+            }
+        }
+    }
+    else {
+        $lsPart1, $lsPart2 = $TerminalLine.Split(" ")
+        if ($lsPart1 -ne "dir") {
+            $Files.Add($Path + $lsPart2, [int]$lsPart1)
+        }
+    }
+}
+
+foreach ($File in $Files.Keys) {
+    $FileParts = $File.Split("/")
+    for ($i = 0; $i -lt $FileParts.Count - 1; $i++) {
+        $currentFolder = ($FileParts[0..$i] -join "/") + "/"
+        $Folders[$currentFolder] += $Files[$File]  }
+}
+
+foreach ($Folder in $Folders.Values) {
+    if ($Folder -le $TotalSizeLimit) {$TotalSize += $Folder}
+}
+
+Write-Host "Total size of at most $TotalSizeLimit is $TotalSize"
+# Correct answer = 1544 (95437 for testdata)
---
+++ 2022/Day07/Part2.ps1
@@ -0,0 +1,58 @@
+Get-MyVariables | Remove-Variable -ErrorAction SilentlyContinue
+[System.Console]::Clear()
+
+[string] $Path = "/" # Make sure to always end Path with a /
+[hashtable] $Files = @{}
+[hashtable] $Folders = @{}
+[int] $TotalSizeLimit = 100000
+[int] $TotalSize = 0
+
+# $Data = Get-Content -Path $PSScriptRoot\DataDemo.txt -ErrorAction Stop
+$Data = Get-Content -Path $PSScriptRoot\Data.txt -ErrorAction Stop
+
+foreach ($TerminalLine in $Data) {
+    if ($TerminalLine.StartsWith("$")) {
+        if ($TerminalLine.StartsWith("$ cd")) {
+            $PathChange = $TerminalLine.Split(" ")[2]
+            switch ($PathChange) {
+                "/" {
+                    $Path = "/"
+                    break
+                }
+                ".." {
+                    if ($Path -ne "/") {
+                        $SecondToLastSlash = $Path.Substring(0, $Path.Length - 1).LastIndexOf("/")
+                        $Path = $Path.Substring(0, $SecondToLastSlash) + "/"
+                    }
+                }
+                Default {
+                    $Path += $PathChange + "/"
+                }
+            }
+        }
+    }
+    else {
+        $lsPart1, $lsPart2 = $TerminalLine.Split(" ")
+        if ($lsPart1 -ne "dir") {
+            $Files.Add($Path + $lsPart2, [int]$lsPart1)
+        }
+    }
+}
+
+foreach ($File in $Files.Keys) {
+    $FileParts = $File.Split("/")
+    for ($i = 0; $i -lt $FileParts.Count - 1; $i++) {
+        $currentFolder = ($FileParts[0..$i] -join "/") + "/"
+        $Folders[$currentFolder] += $Files[$File]  }
+}
+
+$UsedSpace = $Folders["/"]
+$UsedSpaceNeeded = $UsedSpace - 30000000
+foreach ($FolderSize in $Folders.Values | Sort-Object) {
+    if ($FolderSize -ge $UsedSpaceNeeded) {
+        $FolderSize
+        break
+    }
+}
+
+# Correct answer = 1544 (95437 for testdata)
